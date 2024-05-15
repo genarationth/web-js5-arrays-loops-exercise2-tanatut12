dbcc4ff8524ef01941623ea416673ffea81915f6
tanatut12 <tanatut12@gmail.com>
2024-05-15 14:53:36 +0700

    finish

---
+++ script.js
@@ -0,0 +1,9 @@
+const people = ["sofia","David","juan"]
+console.log(people)
+people.shift(0)
+people.push("Sara","Augustin")
+console.log(people)
+people.push("Elena")
+people.splice(1,0,"Reneta")
+console.log(people)
+
